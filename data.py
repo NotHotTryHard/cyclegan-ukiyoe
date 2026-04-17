e0d7e6bbc59c034af5a8a7ae4f3759f781b8dcfa
Mike <michel200212345+github@gmail.com>
2026-04-17 03:10:47 +0300

    Added dataset functions

---
+++ data.py
@@ -0,0 +1,129 @@
+import os
+from dataclasses import dataclass
+
+import cv2
+import numpy as np
+import torch
+import torchvision.transforms as tr
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+
+
+class ImageDatasetNoLabel(Dataset):
+    def __init__(self, class_path, transforms=None):
+        super().__init__()
+        class_label = class_path.split('/')[-1]
+        self.img_paths = [os.path.join(class_path, img_name) for img_name in os.listdir(class_path)]
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        img = cv2.imread(self.img_paths[index])[:, :, ::-1]
+        if self.transforms is not None:
+            img = self.transforms(img)
+        return img
+
+    def __len__(self):
+        return len(self.img_paths)
+
+
+@dataclass
+class DatasetsClass:
+    train_a: ImageDatasetNoLabel
+    train_b: ImageDatasetNoLabel
+    test_a: ImageDatasetNoLabel
+    test_b: ImageDatasetNoLabel
+
+
+@dataclass
+class DataLoadersClass:
+    train_a: DataLoader
+    train_b: DataLoader
+    test_a: DataLoader
+    test_b: DataLoader
+
+
+def get_channel_statistics(dataset):
+    """
+    Функция для получения поканальных статистик (среднее и отклонение) по датасету
+    """
+    channel_sum = None
+    channel_sq_sum = None
+
+    pixel_count = 0
+
+    for img in dataset:
+        img = img / 255.0
+        h, w, c = img.shape
+        pixel_count += h * w
+
+        if channel_sum is None:
+            channel_sum = np.zeros(c, dtype=np.float32)
+            channel_sq_sum = np.zeros(c, dtype=np.float32)
+
+        channel_sum += img.sum(axis=(0, 1))
+        channel_sq_sum += (img * img).sum(axis=(0, 1))
+
+    channel_mean = channel_sum / pixel_count
+    channel_std = np.sqrt(np.maximum(channel_sq_sum / pixel_count - channel_mean ** 2, 0))
+    return channel_mean, channel_std
+
+
+def get_transforms(channel_mean, channel_std, **hyperparams):
+    train_transform = tr.Compose([
+        tr.ToPILImage(),
+        tr.Resize((hyperparams['upscale_size'], hyperparams['upscale_size'])),
+        tr.RandomCrop(hyperparams['crop_size']),
+        tr.RandomHorizontalFlip(p=0.5),
+        tr.ToTensor(),
+        tr.Normalize(channel_mean, channel_std)
+    ])
+
+    val_transform = tr.Compose([
+        tr.ToPILImage(),
+        tr.Resize((hyperparams['crop_size'], hyperparams['crop_size'])),
+        tr.ToTensor(),
+        tr.Normalize(channel_mean, channel_std)
+    ])
+
+    def de_normalize(image):
+        mean = torch.tensor(channel_mean, device=image.device).view(3, 1, 1)
+        std = torch.tensor(channel_std, device=image.device).view(3, 1, 1)
+        return (image * std + mean).clamp(0, 1).permute(1, 2, 0).detach().cpu().numpy()
+
+    return train_transform, val_transform, de_normalize
+
+
+def show_examples(dataset, transform, de_norm, num_per_image=3, image_index=0, title=""):
+    fig, ax = plt.subplots(1, 1 + num_per_image, figsize=(5 * (1 + num_per_image), 5))
+
+    image = dataset[image_index]
+
+    plt.suptitle(title, y=0.95)
+
+    plt.subplot(1, 1 + num_per_image, 1)
+    plt.imshow(image)
+    plt.title("original")
+
+    for i in range(num_per_image):
+        plt.subplot(1, 1 + num_per_image, i + 2)
+        plt.title(f"#{i}")
+        plt.imshow(de_norm(transform(image)))
+    plt.show()
+
+
+def build_datasets(target_folder, train_transform_a, val_transform_a, train_transform_b, val_transform_b):
+    return DatasetsClass(
+        train_a=ImageDatasetNoLabel(os.path.join(target_folder, "trainA"), transforms=train_transform_a),
+        train_b=ImageDatasetNoLabel(os.path.join(target_folder, "trainB"), transforms=val_transform_b),
+        test_a=ImageDatasetNoLabel(os.path.join(target_folder, "testA"), transforms=val_transform_a),
+        test_b=ImageDatasetNoLabel(os.path.join(target_folder, "testB"), transforms=val_transform_b),
+    )
+
+
+def build_dataloaders(ds: DatasetsClass, batch_size=50):
+    return DataLoadersClass(
+        train_a=DataLoader(ds.train_a, batch_size=batch_size, shuffle=True, drop_last=True),
+        train_b=DataLoader(ds.train_b, batch_size=batch_size, shuffle=True, drop_last=True),
+        test_a=DataLoader(ds.test_a, batch_size=batch_size, shuffle=False, drop_last=True),
+        test_b=DataLoader(ds.test_b, batch_size=batch_size, shuffle=False, drop_last=True),
+    )
---
+++ download.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import zipfile
+
+import requests
+import matplotlib.pyplot as plt
+from torchvision import datasets
+from tqdm.auto import tqdm
+
+
+def download_and_preview(dataset_name, dataset_folder="data", num_images_per_split=5, keep_zip=False):
+    print(f"Dataset '{dataset_name}'")
+    url = f"http://efrosgans.eecs.berkeley.edu/cyclegan/datasets/{dataset_name}.zip"
+    download_path = os.path.join(dataset_folder, f"{dataset_name}.zip")
+    target_folder = os.path.join(dataset_folder, dataset_name)
+
+    os.makedirs(dataset_folder, exist_ok=True)
+
+    print("Loading zip file...", end="", flush=True)
+    if not os.path.isfile(download_path) or os.path.exists(target_folder):
+        with requests.get(url, stream=True, timeout=60) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get("Content-Length", 0))
+            with open(download_path, "wb") as f, tqdm(
+                total=total_size if total_size > 0 else None,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+                desc=f"{dataset_name}.zip",
+                leave=False,
+            ) as pbar:
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(len(chunk))
+    print(" --> done!")
+
+    print("Unziping...", end="", flush=True)
+    if os.path.exists(target_folder):
+        shutil.rmtree(target_folder)
+    with zipfile.ZipFile(download_path, "r") as zf:
+        zf.extractall(dataset_folder)
+    print(" --> done!")
+
+    if not keep_zip and os.path.isfile(download_path):
+        os.remove(download_path)
+
+    print(f"Provided splits: {os.listdir(target_folder)}")
+    dataset = datasets.ImageFolder(target_folder)
+
+    inds_to_show = {i: [] for i, _ in enumerate(dataset.classes)}
+    classes_full = 0
+    for dataset_ind in range(len(dataset)):
+        _, split_ind = dataset[dataset_ind]
+        if len(inds_to_show[split_ind]) == num_images_per_split:
+            continue
+        inds_to_show[split_ind].append(dataset_ind)
+        if len(inds_to_show[split_ind]) == num_images_per_split:
+            classes_full += 1
+        if classes_full == len(dataset.classes):
+            break
+
+    for split_name in sorted(dataset.classes):
+        split_ind = dataset.class_to_idx[split_name]
+        print(f"Split '{split_name}' of dataset '{dataset_name}'", end="")
+        split_folder = os.path.join(target_folder, split_name)
+        print(f" --> size: {len(os.listdir(split_folder))}")
+
+        plt.subplots(1, num_images_per_split, figsize=(5 * num_images_per_split, 5))
+        plt.suptitle(f"{dataset_name} ~ {split_name}", y=0.95)
+        for i, dataset_ind in enumerate(inds_to_show[split_ind]):
+            plt.subplot(1, num_images_per_split, i + 1)
+            plt.imshow(dataset[dataset_ind][0])
+            plt.xticks([])
+            plt.yticks([])
+        plt.show()
+
+    print("\n----------------------------\n")
+    return target_folder
+
+
+if __name__ == "__main__":
+    download_and_preview("ukiyoe2photo")
